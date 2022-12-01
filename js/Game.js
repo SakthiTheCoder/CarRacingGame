4ea3512bf8792709c5a12afb89f16b5eae290cd1
SakthiTheCoder <99602340+SakthiTheCoder@users.noreply.github.com>
2022-12-01 17:11:31 +0000

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,63 @@
+class Game {
+  constructor() { }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function (data) {
+      gameState = data.val();
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+    car1 = createSprite(width/2-100,height-50,)
+    car1.addImage(car1_img)
+    car2 = createSprite(width/2+100,height-50,)
+    car2.addImage(car2_img)
+    car1.scale = 0.1;
+    car2.scale = 0.1;
+    cars = [car1,car2]
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play() {
+    this.handleElements()
+    Player.getPlayersInfo()
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6)
+      var index =0 
+      for(var plr in allPlayers){
+        index = index +1
+        var x = allPlayers [plr].positionX
+        var y =  height - allPlayers [plr].positionY
+        cars [index-1].position.x = x
+        cars [index-1].position.y = y
+      }
+      this.handlePlayerControls()
+      drawSprites()
+    }
+
+  }
+handlePlayerControls(){
+  if (keyDown ("up")) {
+    player.positionY+=10
+    
+  }
+}
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    })
+
+
+  }
+}
